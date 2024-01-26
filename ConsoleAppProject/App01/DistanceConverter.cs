3228d9174cdc83ca37fb69ecc182a419a9fbb861
DaRkNiNjA0510 <jakedickinson285@gmail.com>
2024-01-26 14:23:32 +0000

    Changes to DistanceConverter.cs file

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,14 +1,46 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// This class offers methods for converting a 
+    /// given distance measureed in miles to the equivalent
+    /// distance measured in feet
     /// </summary>
     /// <author>
-    /// Derek version 0.1
+    /// Jake Dickinson version 0.1
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+
+        private double feet;
+
+
+        /// <summary>
+        ///    
+        /// </summary>
+        public void Run()
+        {
+
+        }
+
+        /// <summary>
+        /// Prompt the user to enter the distance in miles
+        /// Input the miles as a double number
+        /// </summary>
+        private void InputMiles()
+        {
+        }
+
+        private void CalculateFeet()
+        {
+        }
+
+        private void OutputFeet()
+        {
+        }
+
     }
 }
