5561dc036a5ab5173c7a26eaef0e726508080cfa
DaRkNiNjA0510 <jakedickinson285@gmail.com>
2024-02-02 10:15:03 +0000

    updated the coding from app1 task 2 (02/02/2024)

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -12,21 +12,47 @@ namespace ConsoleAppProject.App01
     /// <author>
     /// Jake Dickinson version 0.1
     /// </author>
+    /// 
     public class DistanceConverter
     {
+        public const int FEET_IN_MILES = 5280;
+
+        public const double METERS_IN_MILES = 1609.34;
+
         private double miles;
 
         private double feet;
 
+        private double meters;
+
 
         /// <summary>
-        ///    
+        ///   This method will input the distance measured in miles
+        ///   calculate the same distance in feet, and output the 
+        ///   distance in feet.
         /// </summary>
-        public void Run()
+        public void MilesToFeet()
         {
+            OutputHeading();
             InputMiles();
             CalculateFeet();
             OutputFeet();
+        }
+
+        public void FeetToMiles()
+        {
+            OutputHeading();
+            InputFeet();
+            CalculateMiles();
+            OutputMiles();
+        }
+
+        public void MilesToMeters()
+        {
+            OutputHeading();
+            InputMiles();
+            CalculateMeters();
+            OutputMeters();
         }
 
         /// <summary>
@@ -40,9 +66,32 @@ namespace ConsoleAppProject.App01
             miles = Convert.ToDouble(value);
         }
 
+        private void InputMeters()
+        {
+            Console.Write("Please enter the number of meters > ");
+            string value = Console.ReadLine();
+            meters = Convert.ToDouble(value);
+        }
+
+        private void InputFeet()
+        {
+            Console.Write("Please enter the number of feet > ");
+            string value = Console.ReadLine();
+            feet = Convert.ToDouble(value);
+        }
         private void CalculateFeet()
         {
-            feet = miles * 5280;
+            feet = miles * FEET_IN_MILES;
+        }
+
+        private void CalculateMiles()
+        {
+            miles = feet / FEET_IN_MILES;
+        }
+
+        private void CalculateMeters()
+        {
+            meters = miles * METERS_IN_MILES;
         }
 
         private void OutputFeet()
@@ -50,5 +99,22 @@ namespace ConsoleAppProject.App01
             Console.WriteLine(miles + " miles is" + feet + "feet!");
         }
 
+        private void OutputMeters()
+        {
+            Console.WriteLine(miles + "miles is" + meters + "meters!");
+        }
+
+        private void OutputMiles()
+        {
+            Console.WriteLine(feet + "feet is" + miles + "miles!");
+        }
+
+        private void OutputHeading()
+        {
+            Console.WriteLine("\n-------------------------------------");
+            Console.WriteLine("          Convert Miles to Feet        ");
+            Console.WriteLine("            By Jake Dickinson          ");
+            Console.WriteLine("--------------------------------------\n");
+        }
     }
 }
--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -24,7 +24,11 @@ namespace ConsoleAppProject
             Console.WriteLine();
 
             DistanceConverter converter = new DistanceConverter();
-            converter.Run();
+          
+            converter.MilesToFeet();
+            converter.FeetToMiles();
+            converter.MilesToMeters();
+
         }
     }
 }
