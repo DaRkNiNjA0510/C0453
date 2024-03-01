bbc36d6a2e45cbda9997aa1ce1cdf54da843deae
DaRkNiNjA0510 <jakedickinson285@gmail.com>
2024-03-01 10:59:44 +0000

    started coding app03

--- ConsoleAppProject/App03/StudentGrades.cs
+++ ConsoleAppProject/App03/StudentGrades.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks.Dataflow;
 using ConsoleAppProject.Helpers;
 
 namespace ConsoleAppProject.App03
@@ -11,6 +12,32 @@ namespace ConsoleAppProject.App03
     /// </summary>
     public class StudentGrades
     {
+        public string[] Students { get; set; }
 
+        public int[] Marks { get; set; }
+
+        public int[] GradeProfile { get; set; }
+
+        public double Mean { get; set; }
+
+        public int Minimum { get; set; }
+
+        public int Maximum { get; set; }
+
+        public StudentGrades()
+        {
+            Students = new string[]
+                {
+                "Daniel","Dylan","Eric",
+                "Georgia", "Hasan", "Hamza",
+                "Jack", "Liam", "Shan",
+                "Shamial"
+                };
+
+            GradeProfile = new int[(int)Grades.A + 1];
+            Marks = new int[Students.Length];
+
+        }
     }
+
 }
