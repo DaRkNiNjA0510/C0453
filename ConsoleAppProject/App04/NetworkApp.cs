4bf20d981b8b624e2c477edc5f8dd15fec748139
DaRkNiNjA0510 <jakedickinson285@gmail.com>
2024-05-24 11:25:54 +0100

    updated NetworkApp.CS coding

--- ConsoleAppProject/App04/NetworkApp.cs
+++ ConsoleAppProject/App04/NetworkApp.cs
@@ -15,9 +15,12 @@ namespace ConsoleAppProject.App04
 
             string[] choices = new string[]
             {
-                "Post Message", "Post Image", "" +
-                "Display All Posts", "Quit"
+                "Post Message", "Post Image","RemovePost", "" +
+                "Display All Posts","Display Posts By Author",
+                "Display Posts By Dates","Add Comments To Post",
+                "Like/Unlike Post","Quit"
             };
+
             bool wantToQuit = false;
 
             do
@@ -28,8 +31,13 @@ namespace ConsoleAppProject.App04
                 {
                     case 1: PostMessage(); break;
                     case 2: PostImage(); break;
-                    case 3: DisplayAll(); break;
-                    case 4: wantToQuit = true; break;
+                    case 3: RemovePost(); break;
+                    case 4: DisplayAll(); break;
+                    case 5: DisplayByAuthor(); break;
+                    case 6: DisplayByDate(); break;
+                    case 7: AddComment(); break;
+                    case 8: LikePosts(); break;
+                    case 9: wantToQuit = true; break;
                 }
 
             } while (!wantToQuit);
@@ -46,6 +54,26 @@ namespace ConsoleAppProject.App04
         }
 
         private void PostMessage()
+        {
+            throw new NotImplementedException();
+        }
+        private void DisplayByAuthor()
+        {
+            throw new NotImplementedException();
+        }
+        private void RemovePost()
+        {
+            throw new NotImplementedException();
+        }
+        private void DisplayByDate()
+        {
+            throw new NotImplementedException();
+        }
+        private void AddComment()
+        {
+            throw new NotImplementedException();
+        }
+        private void LikePosts()
         {
             throw new NotImplementedException();
         }
